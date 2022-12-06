038f71c081fbd8047fa0099e6bc2ffe01e0e8b79
Tiago Costa <tiagodpv@gmail.com>
2022-12-06 12:05:25 -0500

    add links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -50,6 +50,25 @@ const Navbar = () => {
               <p className='w-[85%] md:w-[90%] py-4'>Let's build something legendary together</p>
             </div>
           </div>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+              <li className='py-4 text-sm'>Home</li>
+              </Link>
+              <Link href='/'>
+              <li className='py-4 text-sm'>About</li>
+              </Link>
+              <Link href='/'>
+              <li className='py-4 text-sm'>Skills</li>
+              </Link>
+              <Link href='/'>
+              <li className='py-4 text-sm'>Projects</li>
+              </Link>
+              <Link href='/'>
+              <li className='py-4 text-sm'>Contact</li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </div>
     </div>//
