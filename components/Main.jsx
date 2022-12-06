69060f53a892dd21395506a67e1cd4ea81c40ae3
Tiago Costa <tiagodpv@gmail.com>
2022-12-06 16:49:21 -0500

    add main

---
+++ components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='w-full h-screen text-center'>
+      <div className='max-w-'>
+        <div>
+          <p>
+            
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
