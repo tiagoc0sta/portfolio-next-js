0388a94dc44b77fe13dd1acb46d60cf0ac6ef7b8
Tiago Costa <tiagodpv@gmail.com>
2022-12-08 12:27:25 -0500

    add skill component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+import Skills from '../components/Skills'
 
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <Navbar/>
       <Main/>
       <About/>
+      <Skills/>
     </div>
   )
 }
