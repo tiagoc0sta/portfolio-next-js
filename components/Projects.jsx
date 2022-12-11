22a7b4438cf236a39c5bd43127dd80f2e616c83a
Tiago Costa <tiagodpv@gmail.com>
2022-12-10 21:58:57 -0500

    text hover

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -14,8 +14,8 @@ const Projects = () => {
           <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadoy-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to[#709dfff]'>
             <Image className='rounded-xl group-hover:opacity-10' src={propertyImg} alt='/'/>
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-              <h3>Property Finder </h3>
-              <p>Reac JS</p>
+              <h3 className='text-2xl text-white tracking-wider text-center'>Property Finder </h3>
+              <p className='pb-4 pt-2 text-white text-center'>Reac JS</p>
               <Link href='/'>
 
               </Link>
