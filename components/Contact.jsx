d1fa2cf19dc40df4d68d11141764cfce59caa0c0
Tiago Costa <tiagodpv@gmail.com>
2022-12-17 12:00:28 -0500

    add contact menu

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -106,6 +106,13 @@ const Contact = () => {
             </div> 
           </div>        
         </div>
+        <div className='flex justify-center py-12'>
+          <Link href='/'>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <HiOutlineChevronDoubleUp className='text-[#5651e5]' size={30} />
+            </div>          
+          </Link>
+        </div>
       </div>
     </div>
   )
