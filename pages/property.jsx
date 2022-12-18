7f728a175108dc9d3ec4f77ba88234df1aa16799
Tiago Costa <tiagodpv@gmail.com>
2022-12-18 00:21:53 -0500

    proerty page

--- pages/property.jsx
+++ pages/property.jsx
@@ -1,8 +1,19 @@
+import Image from 'next/legacy/image'
 import React from 'react'
+import propertImg from '../public/assets/projects/property.jpg'
 
 const property = () => {
   return (
-    <div>property</div>
+    <div className='w-full'>
+      <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'/>
+          <Image className='absolute z-1' layout='fill' objectFit='cover' src={propertImg} alt='/'/>
+          <div className='absolute top-[70%] max-w[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10  p-2'>
+            <h2 className='py-2'>Property Finder</h2>
+            <h3>React JS / Tailwind / Firebase</h3>
+          </div>
+      </div>
+    </div>
   )
 }
 
