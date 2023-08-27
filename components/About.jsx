8c082ac814703b57e2d577fec776e9c53ed5084f
Tiago Costa <tiagodpv@gmail.com>
2023-08-27 19:08:54 -0400

    add last

--- components/About.jsx
+++ components/About.jsx
@@ -1,32 +1,36 @@
-import Image from 'next/image'
-import React from 'react'
-import ChrisImg from '../public/assets/chris.jpg'
+import Image from 'next/image';
+import React from 'react';
+import ChrisImg from '../public/assets/chris.jpg';
+import Link from 'next/link';
 
 const About = () => {
   return (
     <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5] font-bold'>About</p>
+          <p className='uppercase text-xl tracking-widest text-[#5651e5] font-bold'>
+            About
+          </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600 font-bold'>I am an UX Researcher</p>
-          <p className='py-2 text-gray-600 '>Hey!
-            I am Christtiane, an UX Researcher!
-            Currently, I am working as a UX Researcher on Creditas Fintech, expanding my training in the world of research based on user pain points.
+          <p className='py-2 text-gray-600 '>
+            Hey! I am Christtiane, an UX Researcher! Currently, I am working as
+            a UX Researcher on Creditas Fintech, expanding my training in the
+            world of research based on user pain points.
           </p>
-          <p className='py-2 text-gray-600 '>Hey!
-            I am Christtiane, an UX Researcher!
-            Currently, I am working as a UX Researcher on Creditas Fintech, expanding my training in the world of research based on user pain points.
-          </p>
-          <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p>
+          <Link
+            href='/#projects'
+            className='py-2 text-gray-600 underline cursor-pointer'
+          >
+            Check out some of my latest projects.
+          </Link>
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image className='rounded-xl' src={ChrisImg} alt="/" />
+          <Image className='rounded-xl' src={ChrisImg} alt='/' />
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- components/Main.jsx
+++ components/Main.jsx
@@ -8,28 +8,48 @@ const Main = () => {
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <p className='upepercase text-sm tracking-widest text-gray-600'></p>
-          <h1 className='py-4 text-gray-700'>
-            Hi, I am <span className='text-[#5651e5]'> Chris</span>
+          <p className='uppercase text-sm tracking-widest text-gray-600'></p>
+          <h1 className='py-4 text-gray-700 text-5xl'>
+            Hi! My name is<span className='text-[#5651e5]'> Chris Costa</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>UX Researcher</h1>
+          <h1 className='py-2 text-gray-700 text-5xl'>I'm a UX Researcher</h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-            I live in Canada and help teams with research a weekly habit and
-            turn those insights into actionable product improvements.
+            Based in Canada, I help teams make research a habit and turn
+            insights into actionable product improvements.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-            <div className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <a
+              className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'
+              href='https://www.linkedin.com/in/christtiane/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               <FaLinkedinIn />
-            </div>
-            <div className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+            </a>
+            <a
+              className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'
+              href='https://github.com/Christtiane'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               <FaGithub />
-            </div>
-            <div className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+            </a>
+            <a
+              className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'
+              href='mailto:christtianec@gmail.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               <AiOutlineMail />
-            </div>
-            <div className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+            </a>
+            <a
+              className='rounded-full shadow-lg shaddow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'
+              href='https://drive.google.com/file/d/15U4nV-owRHf-TVUPV0ruhN8AjjsZx-d4/view?usp=sharing'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               <BsFillPersonLinesFill />
-            </div>
+            </a>
           </div>
         </div>
       </div>
