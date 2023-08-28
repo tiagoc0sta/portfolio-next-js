a95f96c17cfdde0721dfab3456b6732262c109fa
Tiago Costa <tiagodpv@gmail.com>
2023-08-27 21:52:19 -0400

    add conections

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,6 @@ const Main = () => {
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <p className='uppercase text-sm tracking-widest text-gray-600'></p>
           <h1 className='py-4 text-gray-700 text-5xl'>
             Hi! My name is<span className='text-[#5651e5]'> Chris Costa</span>
           </h1>
